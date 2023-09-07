5d523c11478c6b08ab3a6cb07a4ced60229d8058
JDR-SMT <30058666@tafe.wa.edu.au>
2023-09-07 16:03:43 +0800

    Removing Unnecessary Usings

--- ListWikiApp/MainWindow.xaml.cs
+++ ListWikiApp/MainWindow.xaml.cs
@@ -3,20 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
-using Path = System.IO.Path;
 
 namespace ListWikiApp
 {
